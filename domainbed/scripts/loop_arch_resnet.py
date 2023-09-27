76d263d4b0c00b6f14dff14552081d4f5bba7679
Gbetondji Dovonon <jsadovonon@gmail.com>
2023-09-27 13:21:25 +0100

    Update loop_arch_resnet.py

--- domainbed/scripts/loop_arch_resnet.py
+++ domainbed/scripts/loop_arch_resnet.py
@@ -116,6 +116,34 @@ hparams_dict = {
                 "weight_decay": 2.7643974709171963e-05,
                 "vrex_penalty_anneal_iters": 8,
                 "vrex_lambda": 0.14959251216362196} """,
+        "DANN": """{"batch_size": batchsize, 
+                "class_balanced": false, 
+                "data_augmentation": true, 
+                "lr": 0.00016629177873519647, 
+                "nonlinear_classifier": false, 
+                "arch": "archused", 
+                "resnet_dropout": 0.1,
+                "weight_decay": 1.1975155295174919e-06},
+                "d_steps_per_g_step": 3,
+                "grad_penalty": 2.7663277782638813,
+                "lambda": 51.30168523785897,
+                "lr_d": 7.400983580823492e-05,
+                "lr_g": 3.933474398919139e-05,
+                "mlp_depth": 3,
+                "mlp_dropout": 0.1,
+                "mlp_width": 86,
+                "weight_decay_d": 2.988916639814755e-05,
+                "weight_decay_g": 1.2253150874974055e-05,
+                "beta1": 0.0}""",
+        "CAD": """{"batch_size": batchsize, 
+                "class_balanced": false, 
+                "data_augmentation": true, 
+                "lr": 0.00016629177873519647, 
+                "nonlinear_classifier": false, 
+                "arch": "archused", 
+                "resnet_dropout": 0.1,
+                "weight_decay": 1.1975155295174919e-06
+                "temperature": 0.1}""",
     },
     "SpawriousM2M_hard": {
         "ERM": """{"batch_size": batchsize, 
@@ -215,6 +243,34 @@ hparams_dict = {
                 "weight_decay": 2.7643974709171963e-05,
                 "vrex_penalty_anneal_iters": 8,
                 "vrex_lambda": 0.14959251216362196} """,
+        "DANN": """{"batch_size": batchsize, 
+                "class_balanced": false, 
+                "data_augmentation": true, 
+                "lr": 0.00016629177873519647, 
+                "nonlinear_classifier": false, 
+                "arch": "archused", 
+                "resnet_dropout": 0.1,
+                "weight_decay": 1.1975155295174919e-06},
+                "d_steps_per_g_step": 1,
+                "grad_penalty": 3.044417571888086,
+                "lambda": 0.04765466589023375,
+                "lr_d": 0.00012638696055528178,
+                "lr_g": 2.6800465071227878e-05,
+                "mlp_depth": 4,
+                "mlp_dropout": 0.1,
+                "mlp_width": 509,
+                "weight_decay_d": 0.006008142228932282,
+                "weight_decay_g": 6.355656257759041e-05,
+                "beta1": 0.0}""",
+        "CAD": """{"batch_size": batchsize, 
+                "class_balanced": false, 
+                "data_augmentation": true, 
+                "lr": 0.00016629177873519647, 
+                "nonlinear_classifier": false, 
+                "arch": "archused", 
+                "resnet_dropout": 0.1,
+                "weight_decay": 1.1975155295174919e-06
+                "temperature": 0.1}""",
     },
 }
 
@@ -225,8 +281,8 @@ hparams_dict["SpawriousM2M_easy"] = hparams_dict["SpawriousM2M_hard"]
 hparams_dict["SpawriousM2M_medium"] = hparams_dict["SpawriousM2M_hard"]
 
 count = 0
-for arch in ["resnet50"]:
-    for algo in ["CutMix"]:
+for arch in ["resnet50"]: # or resnet50_nopretraining 
+    for algo in ["CutMix"]: # add CAD and DANN
         for dataset in [
             "SpawriousO2O_easy",
             "SpawriousO2O_medium",
