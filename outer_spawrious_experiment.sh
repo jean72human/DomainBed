16aca85df94006f26bbcd48427d474e2c077649a
zcahaly <zcahaly@login12.myriad.ucl.ac.uk>
2023-10-20 17:26:57 +0100

    running away

--- outer_spawrious_experiment.sh
+++ outer_spawrious_experiment.sh
@@ -73,16 +73,33 @@ declare -a PYTHON_COMMANDS=(
     "python -m domainbed.scripts.loop_arch_resnet --dataset_group C --algorithm ERM --architecture resnet50 --da_bool True --da_strategy LISA-CutMix"
 )
 
+# Initialize a counter for the loop
+counter=0
 
 # Iterate through the list of Python commands
 for CMD in "${PYTHON_COMMANDS[@]}"; do
 
-    # Parse the command to generate an appropriate job name for -N
-    DATASET_GROUP=$(echo $CMD | grep -oP '--dataset_group \K\w+')
-    ALGORITHM=$(echo $CMD | grep -oP '--algorithm \K\w+')
-    ARCHITECTURE=$(echo $CMD | grep -oP '--architecture \K\w+')
-    DA_BOOL=$(echo $CMD | grep -oP '--da_bool \K\w+')
-    DA_STRATEGY=$(echo $CMD | grep -oP '--da_strategy \K\w+')
+    ((counter++))
+
+    # Check if 10 iterations have passed
+    if [ $((counter % 10)) -eq 0 ]; then
+        # Pause for 1 hour
+        sleep 1h
+    fi
+
+    DATASET_GROUP=$(echo $CMD | awk -F '--dataset_group ' '{split($2, a, " "); print a[1]}')
+    ALGORITHM=$(echo $CMD | awk -F '--algorithm ' '{split($2, a, " "); print a[1]}')
+    ARCHITECTURE=$(echo $CMD | awk -F '--architecture ' '{split($2, a, " "); print a[1]}')
+    DA_BOOL=$(echo $CMD | awk -F '--da_bool ' '{split($2, a, " "); print a[1]}')
+    DA_STRATEGY=$(echo $CMD | awk -F '--da_strategy ' '{split($2, a, " "); print a[1]}')
+
+    # DATASET_GROUP=$(echo $CMD | sed -n 's/.*--dataset_group \([a-zA-Z0-9_]*\).*/\1/p')
+    # ALGORITHM=$(echo $CMD | sed -n 's/.*--algorithm \([a-zA-Z0-9_]*\).*/\1/p')
+    # ARCHITECTURE=$(echo $CMD | sed -n 's/.*--architecture \([a-zA-Z0-9_]*\).*/\1/p')
+    # DA_BOOL=$(echo $CMD | sed -n 's/.*--da_bool \([a-zA-Z0-9_]*\).*/\1/p'd)
+    # DA_STRATEGY=$(echo $CMD | sed -n 's/.*--da_strategy \([a-zA-Z0-9_]*\).*/\1/p')
+
+
 
     JOB_NAME="${DATASET_GROUP}_${ALGORITHM}_${ARCHITECTURE}_${DA_BOOL}"
     if [ ! -z "$DA_STRATEGY" ]; then
@@ -122,6 +139,7 @@ EOL
     qsub $INNER_SCRIPT
 
     # Optional: Remove the inner script file after submission (uncomment if you want to enable this)
-    # rm $INNER_SCRIPT
+    rm $INNER_SCRIPT
 
 done
+
--- spawrious_experiment.sh
+++ spawrious_experiment.sh
@@ -2,7 +2,7 @@
 #$ -l h_rt=42:00:00
 #$ -l gpu=1
 #$ -pe smp 16
-#$ -N <insert appropriate name>
+#$ -N tester_for_script
 #$ -R y
 #$ -ac allow=E,F
 #$ -S /bin/bash
@@ -27,7 +27,7 @@ nvidia-smi
 # python -m domainbed.scripts.loop_arch_resnet --dataset_group A --algorithm ERM --architecture resnet50-nopretraining --da_bool True --da_strategy LISA-CutMix
 
 # # Group B, ERM algorithm
-# python -m domainbed.scripts.loop_arch_resnet --dataset_group B --algorithm ERM --architecture resnet50-nopretraining --da_bool False
+python -m domainbed.scripts.loop_arch_resnet --dataset_group B --algorithm ERM --architecture resnet50-nopretraining --da_bool False
 # python -m domainbed.scripts.loop_arch_resnet --dataset_group B --algorithm ERM --architecture resnet50-nopretraining --da_bool True --da_strategy random_shuffle-Mixup
 # python -m domainbed.scripts.loop_arch_resnet --dataset_group B --algorithm ERM --architecture resnet50-nopretraining --da_bool True --da_strategy random_shuffle-CutMix
 # python -m domainbed.scripts.loop_arch_resnet --dataset_group B --algorithm ERM --architecture resnet50-nopretraining --da_bool True --da_strategy LISA-Mixup
